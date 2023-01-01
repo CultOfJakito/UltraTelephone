67065aa59c2541309669e2066085f9f36a551989
Daniel Murray <21901746@etu.unicaen.fr>
2023-01-01 14:21:30 +0100

    Did the things. Modified csproj to make generated mod file/folder structure a lot easier to drag-drop onto the game files.

---
+++ Patches/ClearwaterPatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace UltraTelephone.Patches
+{
+    [HarmonyPatch(typeof(LaughingSkull), "Start")]
+    public class ClearwaterPatch
+    {
+        [HarmonyPostfix]
+        public static void doOneThing(ref LaughingSkull __instance, ref AudioSource ___aud)
+        {
+            ___aud = AudioSwapper.SwapClipWithFile(___aud);
+            ___aud.Play();
+            
+        }
+    }
+    
+    [HarmonyPatch(typeof(LaughingSkull), "PlayAudio")]
+    public class ClearwaterPatchAudio
+    {
+        [HarmonyPrefix]
+        public static bool doOtherThing(ref AudioSource ___aud)
+        {
+            if(!___aud.isPlaying)
+            {
+                ___aud.Play();
+            }
+            return false;
+
+        }
+    }
+}
