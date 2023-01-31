e0655b39dc5735ea4df6b903467faae2cde9203c
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-01-31 11:31:24 -0500

    final gitignore commit hopefully

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -4,6 +4,6 @@ namespace UltraTelephone
     {
         public const string PLUGIN_GUID = "UltraTelephone";
         public const string PLUGIN_NAME = "UltraTelephone";
-        public const string PLUGIN_VERSION = "1.1.0";
+        public const string PLUGIN_VERSION = "1.0.0";
     }
 }
